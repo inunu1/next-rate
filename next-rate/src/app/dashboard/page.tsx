a6c1c80993ec9c278a82645bf9113ddbed362c80
Inunu1 <inunu1@outlook.com>
2025-10-19 15:28:51 +0900

    レイアウト確認

--- next-rate/src/app/dashboard/page.tsx
+++ next-rate/src/app/dashboard/page.tsx
@@ -10,7 +10,43 @@ export default function DashboardPage() {
 
       <div className={styles.grid}>
         <Link href="/users-management" className={styles.buttonBlue}>
-          管理者画面
+          管理者追加
+        </Link>
+        <Link href="/players-management" className={styles.buttonBlue}>
+          対局管理画面
+        </Link>
+        <Link href="/results-management" className={styles.buttonBlue}>
+          勝敗管理画面
+        </Link>
+      </div>
+
+      <div className={styles.grid}>
+        <Link href="/users-management" className={styles.buttonBlue}>
+          管理者検索
+        </Link>
+        <Link href="/players-management" className={styles.buttonBlue}>
+          対局管理画面
+        </Link>
+        <Link href="/results-management" className={styles.buttonBlue}>
+          勝敗管理画面
+        </Link>
+      </div>
+
+      <div className={styles.grid}>
+        <Link href="/users-management" className={styles.buttonBlue}>
+          管理者編集
+        </Link>
+        <Link href="/players-management" className={styles.buttonBlue}>
+          対局管理画面
+        </Link>
+        <Link href="/results-management" className={styles.buttonBlue}>
+          勝敗管理画面
+        </Link>
+      </div>
+
+      <div className={styles.grid}>
+        <Link href="/users-management" className={styles.buttonBlue}>
+          管理者削除
         </Link>
         <Link href="/players-management" className={styles.buttonBlue}>
           対局管理画面
