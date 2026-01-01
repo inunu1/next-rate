c7680567521e79a745ea6927394666e73b84f421
Inunu1 <inunu1@outlook.com>
2026-01-01 20:31:42 +0900

    Revert "とりあえず"
    This reverts commit f1cf0fc5a3a1b0477cf35518fa1c8f71bf57b8aa.

--- next-rate/src/app/trend/RateChart.tsx
+++ next-rate/src/app/trend/RateChart.tsx
@@ -9,13 +9,7 @@ import {
   ResponsiveContainer,
 } from 'recharts';
 
-// ★ これが必要（TrendClient とは別ファイルなので）
-type RatePoint = {
-  date: string;
-  rate: number;
-};
-
-export default function RateChart({ data }: { data: RatePoint[] }) {
+export default function RateChart({ data }:{ data: RatePoint[] }) {
   return (
     <div style={{ width: '100%', height: 300, marginTop: 20 }}>
       <ResponsiveContainer>
